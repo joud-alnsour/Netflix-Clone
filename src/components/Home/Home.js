5825357c9292ade4bdbc7fbe9091ec4ecb898709
joud-alnsour <alnsour.joud@gmail.com>
2022-02-16 12:49:05 +0200

    done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,33 +1,54 @@
 import React, { useEffect,useState } from "react";
 import MovieList from "../MovieList/MovieList";
+import{Row,Container} from "react-bootstrap";
 function Home() {
-    const [x, setx] = useState();
+    const [movie, setmovie] = useState();
 
     const fetchData = async () => {
         try {
-             
-            const List = await fetch("https://api.themoviedb.org/3/trending/movie/day?api_key=b300cdefad7adfd65b734dc5211bfd15")
- 
+            const List = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=b300cdefad7adfd65b734dc5211bfd15`)
+
             const List2 = await  List.json();
-            setx(List2.results)
-            console.log(List2)
- 
+            let newResult =[];
+            for(let x of List2.results){
+                x['caption']=''
+                x['iscaption']=false
+                newResult.push(x)
+
+            }
+            setmovie(List2.results);
+            console.log(List2);
 
         } catch (error) {
             console.log("error", error);
         }
 
+    };
+    const updateCaptions =(List2,id) =>{
+        let updated =movie.map(move =>{
+            if (movie.id===id){
+                movie.caption=List2.userCaption;
+                movie.iscaption=!movie.iscaption;
+                return movie;
+            }
+            else return movie
+        })
+        setmovie(updated);
     }
-    
-    useEffect(() => { fetchData(); }, [])
+    useEffect(() => { fetchData(); }, []);
 
 
     return (
-        <>
+        <div>
+            home {movie &&(<Container fluid id="main container">
+                <Row id="fiex-row">
+                    <MovieList movies={movie} updateCaptions={updateCaptions}/>
+                </Row>
+            </Container>)}
             <h1>Home Page</h1>
 
-            <MovieList MovieData={x}/>
-        </>
+            
+        </div>
     )
 }
-export default Home
+export default Home;
